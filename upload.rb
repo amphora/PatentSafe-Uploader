e184f49a3cad156c24e0d38d8b136a0a40821cf8
Simon Coles <simonc@amphora-research.com>
2010-10-15 17:09:03 +0100

    Initial slightly hard coded version

--- upload.rb
+++ upload.rb
@@ -8,6 +8,12 @@ require 'httpclient'
 # Command line parser
 require 'optparse'
 
+# Ruby extensions - get from "gem install -r extensions"
+# See http://extensions.rubyforge.org/rdoc/index.html
+require 'extensions/string'
+
+# So we can iterate over directory contents
+require 'find'
 
 USERNAME = "simonc"
 HOST = "https://coles.morescience.com"
@@ -17,24 +23,29 @@ def upload_file(filename)
   result = HTTPClient.post "#{HOST}/submit/pdf.jspa",
     { :authorId => USERNAME, 
       :destination => "searchable", 
-      :pdfContent => File.new(FILENAME) 
+      :pdfContent => File.new(filename) 
     }
   # This should then come back with something like OK:SJCC0100000059
+  puts result.content
+  # Return true or false
   result.content[0...2]=="OK"
 end
 
-
-
-
-# Loop through the contents
-# If it comes back OK then put in Done folder
-# Otherwise put it in Error folder
-# Create the folders if needed
+# Process an entire directory
+def process_directory(directory_name)
+  puts "Directory called on #{directory_name}"
+  Find.find(directory_name) do |f|
+      # Only work on files which end in .pdf
+      if f.ends_with?(".pdf")
+        result = upload_file(f)
+        puts "Uploaded #{f}, result = #{result}"
+    end
+  end
+end
 
 # At the moment take one argument, which is the directory to process
 opts = OptionParser.new
 rest = opts.parse(ARGV)
 directory_name = rest[0]
-
-
-puts upload_file(FILENAME)
+# Start with the first directory
+process_directory(directory_name)
